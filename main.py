f6d58a750810a5385c5d075879fcc9ca2db809f3
NPlanche <89564236+NPlanche@users.noreply.github.com>
2023-03-28 00:27:56 -0400

    changed base url

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ def index():
                 urlBase = 'https://storage.googleapis.com/project2database/static/image/'
                 image_name = image_url[61:len(image_url)]
                 
-                index_html += "<a href='http://127.0.0.1:8080/?image="+ image_name +"'><img class='image' src='" + blob.public_url + "'></a>"
+                index_html += "<a href='"+ base_url +"/?image="+ image_name +"'><img class='image' src='" + blob.public_url + "'></a>"
 
                 
                 
